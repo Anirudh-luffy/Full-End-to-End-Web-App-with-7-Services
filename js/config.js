d21bab17764c87cc5f80ca4e095647d8ed7999c8
Anirudh-luffy <150931895+Anirudh-luffy@users.noreply.github.com>
2025-08-13 13:42:59 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_7N1dfung1', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4c7vtk8388k598a9taosqhi9m4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_NGlWBQghn', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '58dai34rl1u6qibf5beoukvvn0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://3fpacat4nf.execute-api.ap-south-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
